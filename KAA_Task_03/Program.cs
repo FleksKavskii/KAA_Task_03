23be6a9099e708605b71047cc11d065c94691410
FleksKavskii <66283133+FleksKavskii@users.noreply.github.com>
2020-11-16 17:56:51 +0300

    KAA_Task_03

---
+++ KAA_Task_03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KAA_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = "password";
+            string userPass;
+            int attempt = 3;
+            Console.WriteLine("Введите пароль: ");
+            for (attempt= 3; attempt >= 1; --attempt)
+            {
+                userPass = Console.ReadLine();
+                if (userPass == pass)
+                {
+                    Console.WriteLine("seсret message");
+                    break;
+                }
+                else
+                    Console.WriteLine("Повторите попытку: ");
+                    
+            }
+        }
+    }
+}
